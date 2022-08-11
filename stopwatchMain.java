ba27df5d45c27f6b9e92eb93c9088ddc52289804
Weibler23 <103393943+Weibler23@users.noreply.github.com>
2022-08-11 08:08:26 -0600

    Create stopwatchMain.java

---
+++ stopwatchMain.java
@@ -0,0 +1,10 @@
+// stopwatchMain.java
+// Adam Weibler
+// Java assignment to create a stopwatch
+// Created: 8/11/2022
+
+public class stopwatchMain {
+  public static void main(String[] args) throws Exception {
+  System.out.println (" DEBUG:: Running 'stopwatchMain.java' ");
+  }
+}
